45a58a8ae6ea81d426191b614000a766c1dd2f25
dafinaans19 <dafinaans19@gmail.com>
2026-05-22 14:15:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,6 @@ st.badge("Success", icon=":material/check:", color="green")
 st.markdown(
     ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
 )
+import streamlit as st
+
+st.text("This is text\n[and more text](that's not a Markdown link).")
